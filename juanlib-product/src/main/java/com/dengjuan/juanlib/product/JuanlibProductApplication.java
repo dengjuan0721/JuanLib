71ffab85b933144ac4d7e43edb45175becd1c6df
dengjuan0721 <dengjuan0721@gmail.com>
2024-06-05 21:20:14 +0800

    initialize test 1

---
+++ juanlib-product/src/main/java/com/dengjuan/juanlib/product/JuanlibProductApplication.java
@@ -0,0 +1,13 @@
+package com.dengjuan.juanlib.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuanlibProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuanlibProductApplication.class, args);
+    }
+
+}
